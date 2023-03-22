28a657a2578d6678e77343e811ea18915b57d7d5
Anjan Roy <hello@itzmeanjan.in>
2023-03-22 11:09:10 +0400

    add benchmark code for K12 extendable output function
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ benches/kangarootwelve.rs
@@ -0,0 +1,46 @@
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion, Throughput};
+use kangarootwelve::KangarooTwelve;
+use rand::{thread_rng, RngCore};
+
+fn k12<const MLEN: usize, const CSTRLEN: usize, const DLEN: usize>(c: &mut Criterion) {
+    let mut rng = thread_rng();
+
+    let mut group = c.benchmark_group("K12");
+    group.throughput(Throughput::Bytes(MLEN as u64));
+
+    group.bench_function(&format!("{}/{} (cached)", MLEN, DLEN), |bench| {
+        let mut msg = vec![0u8; MLEN];
+        let mut cstr = vec![0u8; CSTRLEN];
+        let mut dig = vec![0u8; DLEN];
+
+        rng.fill_bytes(&mut msg);
+        rng.fill_bytes(&mut cstr);
+
+        bench.iter(|| {
+            let mut hasher = KangarooTwelve::hash(black_box(&msg), black_box(&cstr));
+            hasher.squeeze(black_box(&mut dig));
+        });
+    });
+    group.bench_function(&format!("{}/{} (random)", MLEN, DLEN), |bench| {
+        let mut msg = vec![0u8; MLEN];
+        let mut cstr = vec![0u8; CSTRLEN];
+        let mut dig = vec![0u8; DLEN];
+
+        rng.fill_bytes(&mut msg);
+        rng.fill_bytes(&mut cstr);
+
+        bench.iter_batched(
+            || (msg.clone(), cstr.clone()),
+            |(msg, cstr)| {
+                let mut hasher = KangarooTwelve::hash(black_box(&msg), black_box(&cstr));
+                hasher.squeeze(black_box(&mut dig));
+            },
+            BatchSize::SmallInput,
+        );
+    });
+
+    group.finish();
+}
+
+criterion_group!(kangarootwelve, k12<32, 0, 32>);
+criterion_main!(kangarootwelve);
