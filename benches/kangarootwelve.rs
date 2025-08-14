825ce4cdf2234da7e0ed97cd1cd256b3b8908811
Anjan Roy <hello@itzmeanjan.in>
2025-08-14 11:34:25 +0400

    Use `divan` for benchmarking
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- benches/kangarootwelve.rs
+++ benches/kangarootwelve.rs
@@ -1,46 +1,87 @@
-use criterion::{BatchSize, Criterion, Throughput, black_box, criterion_group, criterion_main};
 use kangarootwelve::KangarooTwelve;
-use rand::{RngCore, thread_rng};
+use rand::Rng;
+use std::{fmt::Debug, time::Duration};
 
-fn k12<const MLEN: usize, const CSTRLEN: usize, const DLEN: usize>(c: &mut Criterion) {
-    let mut rng = thread_rng();
+#[global_allocator]
+static ALLOC: divan::AllocProfiler = divan::AllocProfiler::system();
 
-    let mut group = c.benchmark_group("K12");
-    group.throughput(Throughput::Bytes((MLEN + CSTRLEN) as u64));
-
-    group.bench_function(&format!("{}/{} (cached)", MLEN, DLEN), |bench| {
-        let mut msg = vec![0u8; MLEN];
-        let mut cstr = vec![0u8; CSTRLEN];
-        let mut dig = vec![0u8; DLEN];
-
-        rng.fill_bytes(&mut msg);
-        rng.fill_bytes(&mut cstr);
-
-        bench.iter(|| {
-            let mut hasher = KangarooTwelve::hash(black_box(&msg), black_box(&cstr));
-            hasher.squeeze(black_box(&mut dig));
-        });
-    });
-    group.bench_function(&format!("{}/{} (random)", MLEN, DLEN), |bench| {
-        let mut msg = vec![0u8; MLEN];
-        let mut cstr = vec![0u8; CSTRLEN];
-        let mut dig = vec![0u8; DLEN];
-
-        rng.fill_bytes(&mut msg);
-        rng.fill_bytes(&mut cstr);
-
-        bench.iter_batched(
-            || (msg.clone(), cstr.clone()),
-            |(msg, cstr)| {
-                let mut hasher = KangarooTwelve::hash(black_box(&msg), black_box(&cstr));
-                hasher.squeeze(black_box(&mut dig));
-            },
-            BatchSize::SmallInput,
-        );
-    });
-
-    group.finish();
+fn main() {
+    divan::Divan::default().bytes_format(divan::counter::BytesFormat::Binary).main();
 }
 
-criterion_group!(kangarootwelve, k12<{1*(1 << 10)}, 0, 32>, k12<{2*(1 << 10)}, 0, 32>, k12<{4*(1 << 10)}, 0, 32>, k12<{8*(1 << 10)}, 0, 32>, k12<{16*(1 << 10)}, 0, 32>, k12<{32*(1 << 10)}, 0, 32>, k12<{1*(1 << 20)}, 0, 32>, k12<{2*(1 << 20)}, 0, 32>, k12<{4*(1 << 20)}, 0, 32>, k12<{8*(1 << 20)}, 0, 32>, k12<{16*(1 << 20)}, 0, 32>, k12<{32*(1 << 20)}, 0, 32>);
-criterion_main!(kangarootwelve);
+fn bytes_to_human_readable(bytes: usize) -> String {
+    let units = ["B", "KB", "MB", "GB", "TB"];
+    let mut bytes = bytes as f64;
+    let mut unit_index = 0;
+
+    while bytes >= 1024.0 && unit_index < units.len() - 1 {
+        bytes /= 1024.0;
+        unit_index += 1;
+    }
+
+    format!("{:.2} {}", bytes, units[unit_index])
+}
+
+struct K12Config {
+    msg_byte_len: usize,
+    digest_byte_len: usize,
+}
+
+impl Debug for K12Config {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!(
+            "Hashing {} message, producing {} digest",
+            bytes_to_human_readable(self.msg_byte_len),
+            bytes_to_human_readable(self.digest_byte_len)
+        ))
+    }
+}
+
+const ARGS: &[K12Config] = &[
+    K12Config {
+        msg_byte_len: 1usize << 5,
+        digest_byte_len: 32,
+    },
+    K12Config {
+        msg_byte_len: 1usize << 6,
+        digest_byte_len: 32,
+    },
+    K12Config {
+        msg_byte_len: 1usize << 10,
+        digest_byte_len: 32,
+    },
+    K12Config {
+        msg_byte_len: 1usize << 15,
+        digest_byte_len: 32,
+    },
+    K12Config {
+        msg_byte_len: 1usize << 20,
+        digest_byte_len: 32,
+    },
+    K12Config {
+        msg_byte_len: 1usize << 25,
+        digest_byte_len: 32,
+    },
+    K12Config {
+        msg_byte_len: 1usize << 30,
+        digest_byte_len: 32,
+    },
+];
+
+#[divan::bench(args = ARGS, max_time = Duration::from_secs(100), skip_ext_time = true)]
+fn k12(bencher: divan::Bencher, k12_config: &K12Config) {
+    bencher
+        .counter(divan::counter::BytesCount::new(k12_config.msg_byte_len + k12_config.digest_byte_len))
+        .with_inputs(|| {
+            let mut rng = rand::thread_rng();
+
+            let msg = (0..k12_config.msg_byte_len).map(|_| rng.r#gen()).collect::<Vec<u8>>();
+            let dig = vec![0u8; k12_config.digest_byte_len];
+
+            (msg, dig)
+        })
+        .bench_refs(|(msg, dig)| {
+            let mut hasher = KangarooTwelve::hash(divan::black_box(msg), divan::black_box(&[]));
+            hasher.squeeze(divan::black_box(dig));
+        });
+}
