ce200938934bfb9f73461a93500e7e61d932411b
Anjan Roy <hello@itzmeanjan.in>
2026-07-15 16:22:54 +0300

    get  rid of magic constants
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/cuda.rs
+++ src/cuda.rs
@@ -52,15 +52,17 @@ unsafe extern "C" {
 type AbsorbFn = unsafe extern "C" fn(*const u8, usize, *const u8, usize, *mut u8) -> i32;
 
 fn absorb_state(ffi: AbsorbFn, msg: &[u8], cstr: &[u8]) -> Result<[u64; 25], CudaError> {
-    let mut bytes = [0u8; 25 * 8];
+    const KECCAK_WORD_BYTE_LENGTH: usize = turboshake::keccak::W / u8::BITS as usize;
+    const KECCAK_STATE_BYTE_WIDTH: usize = turboshake::keccak::LANE_CNT * KECCAK_WORD_BYTE_LENGTH;
 
+    let mut bytes = [0u8; KECCAK_STATE_BYTE_WIDTH];
     let rc = unsafe { ffi(msg.as_ptr(), msg.len(), cstr.as_ptr(), cstr.len(), bytes.as_mut_ptr()) };
     if rc != 0 {
         return Err(CudaError::from_code(rc));
     }
 
-    let mut state = [0u64; 25];
-    for (lane, chunk) in state.iter_mut().zip(bytes.chunks_exact(8)) {
+    let mut state = [0u64; turboshake::keccak::LANE_CNT];
+    for (lane, chunk) in state.iter_mut().zip(bytes.chunks_exact(KECCAK_WORD_BYTE_LENGTH)) {
         *lane = u64::from_le_bytes(chunk.try_into().unwrap());
     }
 
